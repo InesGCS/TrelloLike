4539cd81dddb7f1247e48a90273428d9564c3664
Karen <yong-zhen.lai@epitech.eu>
2022-04-01 00:19:31 +0200

    add popup modal

--- trello-project/src/components/IsCard.vue
+++ trello-project/src/components/IsCard.vue
@@ -1,12 +1,38 @@
 <template>
 <div id="isCard">
-    <p v-html="card.content.rendered"></p>
+    <p @click="() => TogglePopUp('buttonTrigger')" v-html="card.content.rendered"></p>
+    <PopUp
+    v-if="popupTrigger.buttonTrigger"
+    :TogglePopUp="() => TogglePopUp('buttonTrigger')">
+      <h1>My Popup</h1>
+    </PopUp>
 </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import PopUp from '../components/PopUp.vue'
 export default {
-  props: ['card']
+  props: ['card'],
+  components: {
+    PopUp
+  },
+  setup () {
+    const popupTrigger = ref({
+      buttonTrigger: false
+    })
+    const TogglePopUp = (trigger) => {
+      popupTrigger.value[trigger] = !popupTrigger.value[trigger]
+    }
+    return {
+      popupTrigger,
+      TogglePopUp
+    }
+  },
+  data () {
+    return {
+    }
+  }
 }
 
 </script>
---
+++ trello-project/src/components/PopUp.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="popUp">
+    <div class='popUpInner'>
+        <slot />
+        <button class="popUp-close" @click="TogglePopUp('buttonTrigger')">
+            Close PopUp
+        </button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['TogglePopUp']
+}
+
+</script>
+
+<style>
+.popUp {
+    position:fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 99;
+    background-color: rgba(0, 0, 0, 0.2);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.popUpInner {
+    background: #fff;
+    padding: 32px;
+}
+</style>
